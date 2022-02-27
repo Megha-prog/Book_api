3d7e824ee3fe6a7836da9d1a109e19c408c3985c
Megha Dogra <76041569+Megha-prog@users.noreply.github.com>
2022-02-28 01:02:46 +0530

    Add files via upload
    Mysql ---Checking data inserted in table or not

---
+++ bookMysql.sql
@@ -0,0 +1,13 @@
+use ecmo_dev;
+show tables;
+select*from categories;
+
+use book_store;
+show tables;
+select*from books;
+
+create schema store_book;
+use store_book;
+show tables ;
+select*from books;
+desc books;
